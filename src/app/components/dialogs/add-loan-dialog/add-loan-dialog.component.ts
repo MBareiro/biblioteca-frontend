d73bd7e10c988f872454b72f28a37157697836d6
MBareiro <58647378+MBareiro@users.noreply.github.com>
2024-03-13 12:37:43 -0300

    Correccion ortografica en mensaje confimacion

--- src/app/components/dialogs/add-loan-dialog/add-loan-dialog.component.ts
+++ src/app/components/dialogs/add-loan-dialog/add-loan-dialog.component.ts
@@ -92,16 +92,9 @@ export class AddLoanDialogComponent {
       );
       
       if (selectedBook && selectedBook.available <= 0) {
-        /* this.snackBar.open(
-          'No hay copias disponibles de este libro.',
-          'Cerrar',
-          {
-            duration: 4000,
-          }
-        );    */         
-                
         const dialogRefStock = this.dialog.open(StockDialogComponent, {
           data: {
+            title: 'Confirmación',
             message: 'No hay copias disponibles de este libro. ¿Desea registrar una copia?',
             buttonText: {
               ok: 'Sí',
@@ -203,19 +196,75 @@ export class AddLoanDialogComponent {
       );
   }
   // Método para abrir el diálogo de agregar libro
-  openAddBookDialog(): void {
+  openAddBookDialog(event: Event): void {
+    event.stopPropagation();
     const dialogRef = this.dialog.open(AddBookDialogComponent, {
       width: '300px', // Define el ancho del diálogo como prefieras
     });
-
-    dialogRef.afterClosed().subscribe((result: any) => {
+  
+    dialogRef.afterClosed().subscribe((result: Book) => {
       if (result) {
-        // Agrega el nuevo libro a la lista de libros y selecciona automáticamente en el formulario
-        this.books.push(result);
-        this.loanForm.patchValue({
-          books_id: result.id,
-        });
+        // Realizar la búsqueda en la base de datos para verificar si el libro ya existe
+        this.bookService.checkBookExists(result).subscribe(
+          (response: any) => {
+            const exists: boolean = response.exists;
+            if (exists) {
+              // Agregar el ID del libro existente a los datos del libro
+              result.id = response.id;
+  
+              // Actualizar el libro existente
+              this.bookService.updateBookStock(result).subscribe(
+                () => {
+                  this.snackBar.open(
+                    'Stock de libro actualizado con éxito',
+                    'Cerrar',
+                    {
+                      duration: 4000,
+                    }
+                  );
+                  this.loadBooks(); // Actualiza la lista de libros
+                },
+                (error) => {
+                  console.error(
+                    'Error al actualizar el stock del libro:',
+                    error
+                  );
+                }
+              );
+            } else {
+              // Si el libro no existe, crea un nuevo libro
+              this.bookService.addBook(result).subscribe(
+                (createdBook: Book) => {
+                  this.snackBar.open('Libro creado con éxito', 'Cerrar', {
+                    duration: 4000,
+                  });
+                  // Agrega el nuevo libro a la lista de libros
+                  this.books.push(createdBook);
+                  // Establece el valor del formulario en el nuevo libro
+                  this.loanForm.patchValue({
+                    books_id: createdBook.id,
+                  });
+                  this.loadBooks(); // Actualiza la lista de libros
+                },
+                (error) => {
+                  console.error('Error al crear el libro:', error);
+                }
+              );
+            }
+          },
+          (error) => {
+            console.error('Error al verificar si el libro existe:', error);
+            this.snackBar.open(
+              'Error al verificar si el libro existe',
+              'Cerrar',
+              {
+                duration: 4000,
+              }
+            );
+          }
+        );
       }
     });
   }
+  
 }
